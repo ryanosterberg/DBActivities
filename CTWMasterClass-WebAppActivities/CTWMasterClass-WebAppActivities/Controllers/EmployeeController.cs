90b3266afdc82688922fabcb6f4c4faf0bdef617
Mr O <ryan.p.osterberg@gmail.com>
2020-06-21 19:24:19 -0500

    Student and Employee Shells

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeeService service = new EmployeeService();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(service.GetAllEmployees());
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,name,hireDate")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                service.AddEmployee(employee);
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = service.GetEmployeeById((int)id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = service.GetEmployeeById(id);
+            service.DeleteEmployee(employee);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Filtered(int yearsWorked)
+        {
+            return View("Index", service.GetEmployeeByWorkLength(yearsWorked));
+        }
+    }
+}
---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/StudentsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private StudentService service = new StudentService();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(service.GetAllStudents());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = service.GetStudentById((int)id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,name,age,luckyNumber")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                service.AddStudent(student);
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = service.GetStudentById((int)id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,name,age,luckyNumber")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                service.SaveEdits(student);
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = service.GetStudentById((int)id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = service.GetStudentById(id);
+            service.DeleteStudent(student);
+            return RedirectToAction("Index");
+        }
+    }
+}
